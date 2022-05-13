28a2845964b0181ea89f60de78828846b87b5ccb
mauriciov1998 <fideovargastudela1998@gmail.com>
2022-05-13 00:40:46 -0400

    backend - modificado .json

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const rutas = require('./rutas')
 const app = express();
+const path = require('path');
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors());
